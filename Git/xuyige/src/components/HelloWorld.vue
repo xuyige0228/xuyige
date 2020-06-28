72faf80e7e4de1429f6003461a9eca34864052dd
xyg <xuyige0228@163.com>
2020-06-28 15:04:28 +0800

    在dev分支上第一次修改

--- Git/xuyige/src/components/HelloWorld.vue
+++ Git/xuyige/src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+     在dev分支上开发
+      </p>
   </div>
 </template>
 
